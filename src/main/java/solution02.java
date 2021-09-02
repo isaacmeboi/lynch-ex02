5a0b9226f56aad6f283c25408d64a584d86e7967
isaac lynch <thebunnyarcher@gmail.com>
2021-09-02 02:56:38 -0400

    finished the second exercise in one go, pretty simple besides a couple of times where i had to go back into the textbook to see that you could add TWO + which was pretty sick. exercise 02 finished.
    Signed-off-by: isaac lynch <thebunnyarcher@gmail.com>

--- src/main/java/solution02.java
+++ src/main/java/solution02.java
@@ -8,9 +8,9 @@ public class solution02 {
 
     }
 }
-/*also, pretty simple. first call in the java.util library so i can use the scanner
+/*also, pretty simple. first call in the java.util library, so I can use the scanner
  function. declare and create an instance of the scanner function. print out the
  line for them to input the string, read in the input, and then print it. this was
- the hardest part, which was realizing that two + signs could be put into the same funciton.
+ the hardest part, which was realizing that two + signs could be put into the same function.
  first we print the word inputted, and then the length of the function using s.length.
 */
